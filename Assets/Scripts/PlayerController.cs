61e5d98c8954ef39071be4737c14e33ddc24888a
IkeyoBrownyo <56174542+IkeyoBrownyo@users.noreply.github.com>
2019-11-04 13:24:57 -0800

    step-5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 20.0f;
-    private float turnSpeed = 45f;
+    private float turnSpeed = 50f;
     private float horizontalInput;
     private float forwardInput;
 
